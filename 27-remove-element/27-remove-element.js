9252e8ed794e08f136a3f7e355379ab350dda8b9
Kyrillos Hany <kirolousbondok6@gmail.com>
2022-05-24 03:04:56 +0200

    Time: 69 ms (71.15%), Space: 41.4 MB (99.60%) - LeetHub

--- 27-remove-element/27-remove-element.js
+++ 27-remove-element/27-remove-element.js
@@ -1,5 +1,5 @@
 const removeElement = function(nums, val) {
-    for(let i = nums.length - 1; i >= 0; i--){
+    for(let i = nums.length; i >= 0; i--){
         if(nums[i] === val) nums.splice(i, 1)
     }
 };
