71fa304db9654132fc36988e37a8b2a890ca5a23
Kyrillos Hany <kirolousbondok6@gmail.com>
2022-05-24 14:20:20 +0200

    Time: 173 ms (87.90%), Space: 51.4 MB (38.00%) - LeetHub

--- 9-palindrome-number/9-palindrome-number.js
+++ 9-palindrome-number/9-palindrome-number.js
@@ -1,7 +1,3 @@
-/**
- * @param {number} x
- * @return {boolean}
- */
-var isPalindrome = function(x) {
+const isPalindrome = function(x) {
     return x == String(x).split('').reverse().join('')
 };
