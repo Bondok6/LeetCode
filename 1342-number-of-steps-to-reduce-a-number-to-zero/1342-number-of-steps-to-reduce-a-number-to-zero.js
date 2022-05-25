0f17174dc6a5e549e7e5f0b091826c5571393b68
Kyrillos Hany <kirolousbondok6@gmail.com>
2022-05-25 16:31:09 +0200

    Time: 69 ms (70.60%), Space: 41.6 MB (91.45%) - LeetHub

--- 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
+++ 1342-number-of-steps-to-reduce-a-number-to-zero/1342-number-of-steps-to-reduce-a-number-to-zero.js
@@ -1,13 +1,9 @@
 const numberOfSteps = function(num) {
     let count = 0;
     while(num != 0){
-        if(num % 2 == 0){
-            num = num/2;
-            count++;
-        }else{
-            num--;
-            count++;     
-        }
+        if(num % 2 == 0) num = num/2;
+        else num--;       
+        count++;
     }
     return count;
 };
