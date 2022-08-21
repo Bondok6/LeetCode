14a9bd12c212b4010d428368dc577248b9ca3cf3
Kyrillos Hany <kirolousbondok6@gmail.com>
2022-08-21 10:54:15 +0200

    Time: 101 ms (84.91%), Space: 50.2 MB (85.24%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -3,7 +3,7 @@
  * @return {boolean}
  */
 var containsDuplicate = function(nums) {
-    // Worst Case Brute Force O(n2)
+    // Worst Case Brute Force O(n2) space O(1)
 
 //     for(let i = 0; i < nums.length; i++){
 //       let count = 0;
@@ -17,11 +17,20 @@ var containsDuplicate = function(nums) {
     
 //     return false
     
-    // Second solution: Sorting O(n)
-    const sortedArr = nums.sort();
-    console.log(sortedArr)
-    for(let i = 0; i < sortedArr.length; i++){
-        if(sortedArr[i] == sortedArr[i+1]) return true;
+    // Second solution: Sorting O(n) or O(nlogn) because of sort() space O(1)
+//     const sortedArr = nums.sort();
+//     console.log(sortedArr)
+//     for(let i = 0; i < sortedArr.length; i++){
+//         if(sortedArr[i] == sortedArr[i+1]) return true;
+//     }
+    
+//     return false;
+    
+    // Third solution: Set
+    const numSet = new Set();
+    for(let i =0; i < nums.length; i++){
+        if(numSet.has(nums[i])) return true;
+        numSet.add(nums[i]);
     }
     
     return false;
