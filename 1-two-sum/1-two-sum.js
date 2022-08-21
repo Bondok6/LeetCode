d4a89dc31fd39104fb0585de555ab67e7148e236
Kyrillos Hany <kirolousbondok6@gmail.com>
2022-08-21 11:32:28 +0200

    Time: 133 ms (47.02%), Space: 43.5 MB (20.90%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -1,7 +1,17 @@
-const twoSum = function(nums, target) {
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+var twoSum = function(nums, target) {
+    
+    let mapObj = {};
+    
     for(let i = 0; i < nums.length; i++){
-        for(let j = i+1; j < nums.length; j++){
-            if(nums[i] + nums[j] === target) return [i, j];
+        if(target - nums[i] in mapObj){
+            return [mapObj[target - nums[i]], i];
+        }else{
+            mapObj[nums[i]] = i;
         }
     }
 };
