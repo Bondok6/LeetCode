06e99c0c2cd2a43ea60bc34c2c48b69223f035be
Kyrillos Hany <kirolousbondok6@gmail.com>
2022-08-21 10:39:11 +0200

    Time: 202 ms (25.58%), Space: 52.1 MB (10.22%) - LeetHub

--- 217-contains-duplicate/217-contains-duplicate.js
+++ 217-contains-duplicate/217-contains-duplicate.js
@@ -5,16 +5,24 @@
 var containsDuplicate = function(nums) {
     // Worst Case Brute Force O(n2)
 
-    for(let i = 0; i < nums.length; i++){
-      let count = 0;
-        for(let j = 0; j <nums.length; j++){
-            if(nums[i] == nums[j]){
-                count++;
-            }
-        }
-      if(count > 1) return true;
+//     for(let i = 0; i < nums.length; i++){
+//       let count = 0;
+//         for(let j = 0; j <nums.length; j++){
+//             if(nums[i] == nums[j]){
+//                 count++;
+//             }
+//         }
+//       if(count > 1) return true;
+//     }
+    
+//     return false
+    
+    // Second solution: Sorting O(n)
+    const sortedArr = nums.sort();
+    console.log(sortedArr)
+    for(let i = 0; i < sortedArr.length; i++){
+        if(sortedArr[i] == sortedArr[i+1]) return true;
     }
     
-    
-    return false
+    return false;
 };
