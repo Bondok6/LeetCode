bcb62d082f507f4d629607bb86273877da1f82a8
Kyrillos Hany <kirolousbondok6@gmail.com>
2022-08-21 11:36:09 +0200

    Time: 110 ms (63.19%), Space: 42.6 MB (53.96%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -4,6 +4,9 @@
  * @return {number[]}
  */
 var twoSum = function(nums, target) {
+    // [3 , 2 , 4] target = 6
+    // 6 - 3 = 2 Is 2 in obj? No, then add it as property in the object and the value equal the index
+    // yes, then return obj[2], 3
     
     let mapObj = {};
     
